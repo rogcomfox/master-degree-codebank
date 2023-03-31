4de5669f736b0b82735bdc7425b47c1223a2ec53
rogcomfox <joeg.11021996@gmail.com>
2023-03-31 14:49:33 +0700

    update train method

--- comvis/train.py
+++ comvis/train.py
@@ -1,15 +1,18 @@
 # for training data
 import torch # PyTorch package
 import torchvision.datasets as datasets # load datasets
+import torchvision.models as models
 import torchvision.transforms as transforms # transform data
 import torch.nn as nn # basic building block for neural neteorks
 import torch.optim as optim # optimzer
 import torch.utils.data as load_data
-import backbone.main_net as net
+import torch.nn.functional as F
+from tqdm import tqdm
+# import backbone.main_net as net
 
 def train(path_to_train_data, batch_size=4, num_workers=2, num_epochs=50):
     data_transform = transforms.Compose(
-        [transforms.RandomResizedCrop(256), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        [transforms.RandomResizedCrop(256),transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
     #load train data
     trainset = datasets.ImageFolder(root=path_to_train_data, transform=data_transform)
@@ -19,46 +22,31 @@ def train(path_to_train_data, batch_size=4, num_workers=2, num_epochs=50):
     classes = ('Bicycle', 'Bus', 'Boat', 'Car', 'Helicopter', 'Motorcycle', 'Taxi', 'Truck', 'Van')
 
     #loss function
-    criterion = nn.CrossEntropyLoss() #based on paper
-    main_net = net.MainNet()
-    optimizer = optim.SGD(main_net.parameters(), lr=0.01, weight_decay=1e-3, momentum=0.9) #based on paper
+    # criterion = nn.CrossEntropyLoss() #based on paper
+    # main_net = net.MainNet()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = models.resnet50(pretrained=True)
+    model.cuda()
+    optimizer = optim.SGD(model.fc.parameters(), lr=0.01, weight_decay=1e-3, momentum=0.9) #based on paper
 
-    # start training
-    start = torch.cuda.Event(enable_timing=True)
-    end = torch.cuda.Event(enable_timing=True)
-    
-    start.record()
-    
-    for epoch in range(num_epochs):  # loop over the dataset for 50 times (based on paper)
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+    model.train()
+    # model.train()
+    for epoch in range(num_epochs):
+        with tqdm(trainloader, unit="batch") as tepoch:
+            for data, target in tepoch:
+                tepoch.set_description(f"Epoch {epoch}")
+                data, target = data.to(device), target.to(device)
+                optimizer.zero_grad()
+                output = model(data)
+                predictions = output.argmax(dim=1, keepdim=True).squeeze()
+                loss = F.nll_loss(output, target)
+                correct = (predictions == target).sum().item()
+                accuracy = correct / batch_size
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-    # whatever you are timing goes here
-    end.record()
-
-    # Waits for everything to finish running
-    torch.cuda.synchronize()
-
-    print('Finished Training')
-    print(start.elapsed_time(end))  # milliseconds
-
+                loss.backward()
+                optimizer.step()
+                
+                tepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
+            
     #save model
-    torch.save(main_net.state_dict(), './new_model.pth')
+    torch.save(model, './new_model.pth')
