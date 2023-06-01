9fa3a4debd07cf0c5883db317eaa6faaf63a4f1b
rogcomfox <joeg.11021996@gmail.com>
2023-06-02 00:07:58 +0700

    success apply edge in image

--- pcd_new/main_segment.py
+++ pcd_new/main_segment.py
@@ -1,30 +1,64 @@
 import cv2 as cv
+import os
 import argparse
-import numpy as np
-import matplotlib.pyplot as plt
-from utils_segment import kmeans_segment, median_filter, rgb_to_lab
+from utils_segment import kmeans_segment, post_processing, draw_edge, bgr_to_lab, bgr_to_hsv, bgr_to_hls, bgr_to_hsi, watershed_segment
 
-#main method
+#main implementation
 def main_segment():
     img = cv.imread('main_image.png')
-    # normalize image
-    img = cv.resize(img, (400,300))
+    img = cv.resize(img, (800,600))
     #color conversion to lab
-    lab_img, b_channel = rgb_to_lab(img)
-    #segmentation with k-means
-    lab_segment = kmeans_segment(lab_img, 3)
-    b_segment = kmeans_segment(b_channel, 3)
-    b_median = median_filter(b_segment, size=9)
-    lab_median = median_filter(lab_segment, size=9)
-    cv.imshow('ori_img', img)
-    cv.imshow('b_channel only', b_channel)
-    cv.imshow('l_channel=0', lab_img)
-    cv.imshow('b_channel_segment', b_segment)
-    cv.imshow('lab_segment', lab_segment)
-    cv.imshow('b_median', b_median)
-    cv.imshow('lab_median', lab_median)
+    lab_img, lab_to_zero = bgr_to_lab(img)
+    if os.path.exists('lab_img.jpg') == False and os.path.exists('lab_zero.jpg') == False:
+        cv.imwrite('lab_img.jpg', lab_img)
+        cv.imwrite('lab_zero.jpg', lab_to_zero)
+    # because better using l=0, continue segmentation
+    lab_segment = kmeans_segment(lab_to_zero, k=3)
+    opening_lab, canny_lab = post_processing(lab_segment)
+    img_edge = draw_edge(img, canny_lab)
+    cv.imshow('apply_edge', img_edge)
+    # cv.imshow('lab_segment',lab_segment)
+    # cv.imshow('morph_result', opening_lab)
+    # cv.imshow('canny_result', canny_lab)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+# we compare watershed vs kmeans for segmentation task
+def segment_comparison():
+    img = cv.imread('main_image.png')
+    img = cv.resize(img, (800,600))
+    #conversion to lab
+    _, lab_to_zero = bgr_to_lab(img)
+    watershed_result = watershed_segment(lab_to_zero)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+# we compare with several famous color space using kmean segmentation
+def color_space_effect():
+    img = cv.imread('main_image.png')
+    img = cv.resize(img, (800,600))
+    #lab color space (l=0)
+    _, lab_to_zero = bgr_to_lab(img)
+    lab_segment = kmeans_segment(lab_to_zero, k=3)
+    opening_lab, canny_lab = post_processing(lab_segment)
+    #hsv color space (h=0)
+    _, hsv_to_zero = bgr_to_hsv(img)
+    hsv_segment = kmeans_segment(hsv_to_zero)
+    opening_hsv, canny_hsv = post_processing(hsv_segment)
+    #hsl color space (h=0)
+    _, hls_to_zero = bgr_to_hls(img)
+    hls_segment = kmeans_segment(hls_to_zero)
+    opening_hls, canny_hls = post_processing(hls_segment)
+    cv.imshow('morph_lab', opening_lab)
+    cv.imshow('morph_hsv', opening_hsv)
+    cv.imshow('morph_hls', opening_hls)
+    cv.imshow('canny_lab', canny_lab)
+    cv.imshow('canny_hsv', canny_hsv)
+    cv.imshow('canny_hls', canny_hls)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main_segment()
+    # segment_comparison()
+    # color_space_effect()
--- pcd_new/utils_segment.py
+++ pcd_new/utils_segment.py
@@ -1,16 +1,66 @@
+import math
 import cv2 as cv
 import numpy as np
 
-def rgb_to_lab(rgb_img):
-    lab_img = cv.cvtColor(rgb_img, cv.COLOR_RGB2LAB)
+#color space lab
+def bgr_to_lab(rgb_img):
+    lab_img = cv.cvtColor(rgb_img, cv.COLOR_BGR2LAB)
     #set l channel to 0
-    lab_img[:,:,0] = 0
-    l, a, b = cv.split(lab_img)
-    return lab_img, b
+    lab_to_zero = lab_img.copy()
+    lab_to_zero[:,:,0] = 0
+    return lab_img, lab_to_zero
 
-def kmeans_segment(lab_img, k=3):
+#color space hsv
+def bgr_to_hsv(rgb_img):
+    hsv_img = cv.cvtColor(rgb_img, cv.COLOR_BGR2HSV)
+    hsv_to_zero = hsv_img.copy()
+    hsv_to_zero[:,:,0] = 0
+    return hsv_img, hsv_to_zero
+
+#color space hls
+def bgr_to_hls(rgb_img):
+    hls_img = cv.cvtColor(rgb_img, cv.COLOR_BGR2HLS)
+    hls_to_zero = hls_img.copy()
+    hls_to_zero[:,:,0] = 0
+    return hls_img, hls_to_zero
+
+#color space hsi
+def bgr_to_hsi(rgb_img):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        bgr = np.float32(rgb_img)/255
+        blue = bgr[:,:,0]
+        green = bgr[:,:,1]
+        red = bgr[:,:,2]
+
+        #find intensity
+        intensity = np.divide(blue + green + red, 3)
+        # find saturation
+        minimum = np.minimum(np.minimum(red, green), blue)
+        saturation = 1 - (3 / (red + green + blue + 0.001) * minimum)
+        #find hue
+        hue = np.copy(red)
+        for i in range(0, blue.shape[0]):
+            for j in range(0, blue.shape[1]):
+                hue[i][j] = 0.5 * ((red[i][j] - green[i][j]) + (red[i][j] - blue[i][j])) / \
+                            math.sqrt((red[i][j] - green[i][j])**2 + ((red[i][j] - blue[i][j]) * (green[i][j] - blue[i][j])))
+                hue[i][j] = math.acos(hue[i][j])
+
+                if blue[i][j] <= green[i][j]:
+                    hue[i][j] = hue[i][j]
+                else:
+                    hue[i][j] = ((360 * math.pi) / 180.0) - hue[i][j]
+        #merge all
+        hsi_img = cv.merge((hue, saturation, intensity))
+        hsi_to_zero = hsi_img.copy()
+        hsi_to_zero[:,:,0] = 0
+        return hsi_img, hsi_to_zero
+
+def watershed_segment(img):
+    s = 0
+
+def kmeans_segment(img, k=3):
     #reshape image
-    img_pixel = lab_img.reshape((-1, 3))
+    img_pixel = img.reshape((-1, 3))
     img_pixel = np.float32(img_pixel)
 
     # define criteria
@@ -25,34 +75,25 @@ def kmeans_segment(lab_img, k=3):
     )
     centers = np.uint8(centers)
     segmented_data = centers[labels.flatten()]
-    res_img = segmented_data.reshape((lab_img.shape))
+    res_img = segmented_data.reshape((img.shape))
     return res_img
 
-def median_filter(lab_img, size=5):
-    filter_img = cv.medianBlur(lab_img, ksize=size)
-    return filter_img
+def post_processing(img, median_size=5, morph_kernel=5, thresh_low=50, thresh_up=100):
+    #first add median filter
+    filter_img = cv.medianBlur(img, ksize=median_size)
+    #then we using morphological operation (opening)
+    kernel = np.ones((morph_kernel,morph_kernel), np.uint8)
+    erosion_img = cv.erode(filter_img, kernel, iterations=2)
+    dilation_img = cv.dilate(erosion_img, kernel, iterations=3)
+    #then add canny edge operator
+    canny_img = cv.Canny(dilation_img, thresh_low, thresh_up)
+    return dilation_img, canny_img
 
-# def post_process(res_img):
-#     filter_img = cv.medianBlur(res_img, ksize=5)
-#     th, im_th = cv.threshold(filter_img, 220, 255, cv.THRESH_BINARY_INV)
- 
-#     # Copy the thresholded image.
-#     im_floodfill = im_th.copy()
-    
-#     # Mask used to flood filling.
-#     # Notice the size needs to be 2 pixels than the image.
-#     h, w = im_th.shape[:2]
-#     mask = np.zeros((h+2, w+2), np.uint8)
-    
-#     # Floodfill from point (0, 0)
-#     cv.floodFill(im_floodfill, mask, (0,0), 255)
-    
-#     # Invert floodfilled image
-#     im_floodfill_inv = cv.bitwise_not(im_floodfill)
-    
-#     # Combine the two images to get the foreground.
-#     im_out = im_th | im_floodfill_inv
-
-#     #add canny operator
-#     edge_img = cv.Canny(im_out, threshold1=50, threshold2=150)
-#     return edge_img
+# to remove background with edge detector
+def draw_edge(ori_img, canny_mask):
+    h, w = ori_img.shape[:2]
+    #first find contour
+    contours, _ = cv.findContours(canny_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    img_copy = ori_img.copy()
+    result = cv.drawContours(img_copy, contours, -1, (0,255,0), 3)
+    return result
